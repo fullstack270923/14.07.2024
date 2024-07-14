bb0b2a57a3ed60562fa92e9bcca01ca5d84cf326
fullstack270923 <148704300+fullstack270923@users.noreply.github.com>
2024-07-14 18:52:27 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,45 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Employee;
+import com.example.demo.model.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EmployeeRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired private CacheRepositoryImpl cacheRepositoryimpl;
+    public List<Employee> getEmployees() {
+
+        String query = "Select * from company";
+        return jdbcTemplate.query(query, new EmployeeMapper());
+    }
+    public void insertEmployee(Employee employee) {
+        String query = "INSERT INTO company(name, age, address, salary) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(query, employee.getName(), employee.getAge(),
+                employee.getAddress(), employee.getSalary());
+    }
+    public Employee getEmployeeById(Integer id) {
+        if (cacheRepositoryimpl.isKeyExist(String.valueOf(id))) {
+            String emp_as_string = cacheRepositoryimpl.getCacheEntity(String.valueOf(id));
+            System.out.println("Bring in from cache");
+            return null;
+        }
+        String query = "Select * from company where id =?";
+        try
+        {
+            Employee emp = jdbcTemplate.queryForObject(query, new EmployeeMapper(), id);
+            cacheRepositoryimpl.createCacheEntity(String.valueOf(id), emp.toString());
+            return emp;
+        }
+        catch (EmptyResultDataAccessException e) {
+            cacheRepositoryimpl.createCacheEntity(String.valueOf(id), "null");
+            return null;
+        }
+
+    }
+}
